34ff1ae5857b238902788330b4698cfdba779574
Horacio Molina <horaciomolinab0@gmail.com>
2021-04-21 02:58:15 -0400

    3am

---
+++ app/Models/Cliente.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $table='cliente';
+    protected $fillable=[
+        'nombre',
+        'apellidos',
+        'correo',
+        'telefono'
+    ];
+    public $timestamps=false;
+}
---
+++ database/migrations/2021_04_21_065100_create_cliente_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',30);
+            $table->string('apellidos',30);
+            $table->string('correo',50);
+            $table->string('telefono',10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
