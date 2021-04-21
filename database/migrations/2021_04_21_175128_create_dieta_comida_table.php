3259a08a0f8bfff0901e3a044cacd8d1ad38a1ec
Horacio Molina <horaciomolinab0@gmail.com>
2021-04-21 15:29:05 -0400

    falta migrar

---
+++ database/migrations/2021_04_21_175128_create_dieta_comida_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDietaComidaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dieta_comida', function (Blueprint $table) {
+            $table->foreignId('id_dieta');
+            $table->foreignId('id_comida');
+
+
+            $table->primary(['id_dieta', 'id_comida']);
+            $table->foreign('id_dieta')->references('id')->on('dieta');
+            $table->foreign('id_comida')->references('id')->on('comida');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dieta_comida');
+    }
+}
